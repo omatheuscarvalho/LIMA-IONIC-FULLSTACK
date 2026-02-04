a5a7c44842fd3d811f285306c578367f417ecd89
Rayan757575 <rayancatapretastl@gmail.com>
2026-02-03 23:23:33 -0300

    Mudança da formatação de números e datas para o padrão brasileiro

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
-import { importProvidersFrom } from '@angular/core';
+import { importProvidersFrom, LOCALE_ID } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { AuthService } from './app/auth/auth.service';
 import { AuthGuard, NoAuthGuard } from './app/auth/auth.guard';
+
+registerLocaleData(localePt);
 
 bootstrapApplication(AppComponent, {
   providers: [
@@ -18,5 +22,6 @@ bootstrapApplication(AppComponent, {
     AuthService,
     AuthGuard,
     NoAuthGuard,
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
   ],
 });
